124fab3fa39974a5fc3003f1aa873bbee5679dd3
leandro_vandari <leandro_vandari@penguin>
2024-09-21 01:38:49 -0300

    Adicionei varias coisas

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 use interfaces;
-use local_ip_address::local_ip;
-use std::io::{prelude::*, stdin, stdout, BufReader};
+use std::io::{self, prelude::*, stdin, stdout, BufReader};
 use std::net::TcpListener;
 use std::net::TcpStream;
+use std::sync::mpsc;
+use std::thread;
 
 fn main() {
-    println!("{:?}", local_ip());
     let mut meu_nome = String::new();
     print!("Seu nome de usuário: ");
     let _ = stdout().flush();
@@ -19,7 +19,7 @@ fn main() {
     for interface in interfaces {
         for address in &interface.addresses {
             if let Some(mut addr) = address.addr {
-                addr.set_port(7879);
+                addr.set_port(7880);
                 if let Ok(mut stream) = TcpStream::connect(addr) {
                     let mut read_buffer = String::new();
                     let mut buf_reader = BufReader::new(&mut stream);
@@ -30,28 +30,74 @@ fn main() {
                     let nome_outro = read_buffer.trim().to_string();
 
                     stream.write(meu_nome.as_bytes()).unwrap();
-                    println!("Conexão com {}!", nome_outro);
-                    conexoes.push((nome_outro, stream));
+                    stream.set_nonblocking(true).unwrap();
+                    let conexao = Conexao::new(nome_outro, stream);
+                    println!("Conexão com {}!", conexao.nome);
+
+                    conexoes.push(conexao);
                 }
             }
         }
     }
+    let (tx, rec_msg) = mpsc::channel();
+    let reader_thread = thread::spawn(move || {
+        let mut msg = String::new();
 
+        loop {
+            stdin().read_line(&mut msg).unwrap();
+            let _ = tx.send(msg.clone());
+            msg.clear();
+        }
+    });
     loop {
         let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+        listener.set_nonblocking(true).expect("Sei lá");
+
         for stream in listener.incoming() {
-            let mut stream = stream.unwrap();
+            match stream {
+                Ok(mut stream) => {
+                    let mut read_buffer = String::new();
 
-            let mut read_buffer = String::new();
-
-            stream.write(&meu_nome.as_bytes()).unwrap();
-            let mut buf_reader = BufReader::new(&mut stream);
-            buf_reader
-                .read_line(&mut read_buffer)
-                .expect("Primeira mensagem deveria ser o nome de usuário");
-            let nome_outro = read_buffer.trim().to_string();
-            println!("Conexão com {}!", nome_outro);
-            conexoes.push((nome_outro, stream));
+                    stream.write(&meu_nome.as_bytes()).unwrap();
+                    let mut buf_reader = BufReader::new(&mut stream);
+                    buf_reader
+                        .read_line(&mut read_buffer)
+                        .expect("Primeira mensagem deveria ser o nome de usuário");
+                    stream.set_nonblocking(true).expect("Droga");
+                    let nome_outro = read_buffer.trim().to_string();
+                    let conexao = Conexao::new(nome_outro, stream);
+                    println!("Conexão com {}!", conexao.nome);
+                    conexoes.push(conexao);
+                }
+                Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => break,
+                Err(e) => panic!("Error: {e}"),
+            }
         }
+
+        for conexao in &mut conexoes {
+            let mut receber_mensagens = String::new();
+            let mensagem = conexao.conexao.read_to_string(&mut receber_mensagens);
+            println!("{mensagem:?}");
+            if let Ok(tamanho) = mensagem {
+                if tamanho >= 1 {
+                    println!("{}: {}", conexao.nome, receber_mensagens);
+                }
+            }
+            while let Ok(msg) = rec_msg.try_recv() {
+                println!("Mandando {msg}");
+                conexao.conexao.write(&mut msg.as_bytes()).unwrap();
+            }
+        }
+    }
+}
+
+struct Conexao {
+    nome: String,
+    conexao: TcpStream,
+}
+
+impl Conexao {
+    pub fn new(nome: String, conexao: TcpStream) -> Self {
+        Self { nome, conexao }
     }
 }
