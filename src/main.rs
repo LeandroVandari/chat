785e3e533784e6ca1be5c7642baa197fa0064940
Leandro Vandari <bernardokreutz@gmail.com>
2024-09-22 19:57:10 -0300

    Escolha de modo melhorada

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ mod server;
 use comunicacao::utilities::input;
 
 fn main() {
-    let mut modo = Modo::from_string(input("(C)liente ou (S)ervidor? Padrão: Cliente"));
+    let mut modo = Modo::from_string(input("(C)liente ou (S)ervidor? (Padrão: Cliente) "));
     while let Err(e) = modo {
         println!("Erro: {e}");
         modo = Modo::from_string(input("(C)liente ou (S)ervidor? Padrão: Cliente"));
@@ -27,7 +27,7 @@ impl Modo {
     fn from_string(string: String) -> Result<Self, &'static str> {
         if string.to_ascii_lowercase().starts_with("s") {
             Ok(Self::Servidor)
-        } else if string.to_ascii_lowercase().starts_with("c") || string.is_empty() {
+        } else if string.to_ascii_lowercase().starts_with("c") || string.trim().is_empty() {
             Ok(Self::Cliente)
         } else {
             Err("Modo inválido")
