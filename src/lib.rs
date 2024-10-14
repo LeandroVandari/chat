07832f4151b9f5f4ca251278ee092a49c56bdaa8
LeandroVandari <bernardokreutz@gmail.com>
2024-10-13 23:11:17 -0300

    aaaaaalmost works

--- src/lib.rs
+++ src/lib.rs
@@ -53,20 +53,41 @@ impl ChatWindow {
                     .messages
                     .iter()
                     .rev()
-                    .take(message_area.height as usize - 2)
-                    .map(|msg| msg.formatted())
-                    .map(|line| {
+                    .take(message_area.height as usize - 1)
+                    .map(|msg| {
+                        let line = msg.formatted();
                         (
-                            line.spans
+                            {let line_string = line.spans
                                 .iter()
-                                .fold(0, |acc, span| acc + span.content.len())
-                                .div_ceil(message_area.width as usize - 2),
+                                .fold(String::new(), |mut acc, span| {acc.push_str(&span.content); acc});
+
+                            let words = line_string.split_ascii_whitespace();
+                            let bigger_than_line = words.clone().enumerate().filter(|(idx, word)| word.len() > (message_area.height as usize - 2));
+                            if bigger_than_line.clone().peekable().peek().is_none() {
+                                line.spans.iter().fold(0, |acc, span| acc + span.content.len())
+                                .div_ceil(message_area.width as usize - 2)
+                            }
+                            else {
+                            let mut last_bigger_idx: isize = -1;
+                            let words_vec = words.collect::<Vec<_>>();
+                            let mut lines_amount = 0;
+                            for (idx, word) in bigger_than_line {
+                                let lines_before = words_vec[(last_bigger_idx+1) as usize..idx].iter().fold(0, |acc, word| acc + word.len() + 1).div_ceil(message_area.width as usize - 2);
+                                lines_amount += lines_before;
+                                lines_amount += word.len().div_ceil(message_area.width as usize - 2);
+                                last_bigger_idx = idx as isize;
+                            }
+                            lines_amount}
+
+                            },
+
+
                             line,
                         )
                     })
                     .take_while(|(len, _line)| {
                         sum += len;
-                        sum < (message_area.height - 2) as usize
+                        sum <= (message_area.height - 2) as usize
                     })
                     .map(|(_len, line)| line)
                     .collect::<Vec<Line>>();
